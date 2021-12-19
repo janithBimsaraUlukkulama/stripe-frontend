aa76ae1db987a64b59b0747633d4ac0aa029f9b2
janithBimsaraUlukkulama <jbimsaraulukkulama@outlook.com>
2021-12-19 22:34:19 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
-
+import StripeCheckout from "react-stripe-checkout";
+import { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  console.log(process.env.REACT_APP_KEY);
+
+  const [product, setProduct] = useState({
+    name: "ProductName",
+    price: 10,
+    productBy: "productMarketer"
+  });
+
+  const makePayment = token => {
+    const body = { token, product };
+    const headers = {
+      "Content-Type": "application/json"
+    };
+
+    return fetch(`http://localhost:6969/payment`, {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body)
+    }).then(res => {
+      console.log(res);
+      const { status } = res;
+    }).catch(
+      err => console.log(err)
+    )
+}
+
+return (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+      </a>
+
+      <StripeCheckout
+        stripeKey="pk_test_51K8NugJywlKEoracfzQxPzu6pKMOV5tuVLbzqGoNmvnFS39ZzpiHIocNWbdlqwnLvBaaWxawNdc8KCBwFmWNUE0E00rB6hPc8U"
+        token={makePayment}
+        name='Buy product'
+        amount={product.price * 100}
+      >
+        <button className='btn-large pink'>Buy now</button>
+      </StripeCheckout>
+
+    </header>
+  </div>
+);
 }
 
 export default App;
